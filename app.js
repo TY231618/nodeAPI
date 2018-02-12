dc5a2ed4273424a08e28a8efc699aa03bbbc983d
Tony <anthony.c.young18@gmail.com>
2018-02-12 11:41:24 +0000

    added find product by id endpoint

--- app.js
+++ app.js
@@ -17,43 +17,43 @@ app.get('/', (req, res) => {
   res.send('hello');
 });
 
-app.get('/createProducts', (req, res) => {
+// app.get('/createProducts', (req, res) => {
 
-  let productList = [
-    {
-      'name': 'nike top',
-      'productType': 't-shirt',
-      'price': 10.99,
-      'inStock': true,
-      'dateAdded': Date.now()
-    },
-    {
-      'name': 'hat',
-      'productType': 'big hat',
-      'price': 20.99,
-      'inStock': true,
-      'dateAdded': Date.now()
-    },
-    {
-      'name': 'high tops',
-      'productType': 'shoes',
-      'price': 60.99,
-      'inStock': true,
-      'dateAdded': Date.now()
-    },
-    {
-      'name': 'socks',
-      'productType': 'socks',
-      'price': 1.99,
-      'inStock': false,
-      'dateAdded': Date.now()
-    }
-  ];
+//   let productList = [
+//     {
+//       'name': 'nike top',
+//       'productType': 't-shirt',
+//       'price': 10.99,
+//       'inStock': true,
+//       'dateAdded': Date.now()
+//     },
+//     {
+//       'name': 'hat',
+//       'productType': 'big hat',
+//       'price': 20.99,
+//       'inStock': true,
+//       'dateAdded': Date.now()
+//     },
+//     {
+//       'name': 'high tops',
+//       'productType': 'shoes',
+//       'price': 60.99,
+//       'inStock': true,
+//       'dateAdded': Date.now()
+//     },
+//     {
+//       'name': 'socks',
+//       'productType': 'socks',
+//       'price': 1.99,
+//       'inStock': false,
+//       'dateAdded': Date.now()
+//     }
+//   ];
 
-  Product.create(productList, (err, products) => {
-    // if(err) res.sendStatus(500);
-  });
-});
+//   Product.create(productList, (err, products) => {
+//     // if(err) res.sendStatus(500);
+//   });
+// });
 
 app.get('/products', (req, res) => {
 
@@ -61,6 +61,15 @@ app.get('/products', (req, res) => {
     if(err) res.sendStatus(500);
     res.send(products);
   });
+});
+
+app.get('/products/:productId', (req, res) => {
+
+  Product.findById({_id: req.params.productId}, (err, product) => {
+    if(err) res.status(500).send(err);
+
+    res.send(product);
+  })
 });
 
 app.listen(port, (err) => {
