47780368046cf45181b7cb04058d81627f6323a2
Tony <anthony.c.young18@gmail.com>
2018-02-12 11:30:43 +0000

    created dummy data route and get route for product list

--- api/models/productModel.js
+++ api/models/productModel.js
@@ -2,7 +2,7 @@ let mongoose = require('mongoose');
 let Schema = mongoose.Schema;
 
 let productSchema = new Schema({
-  name: String,
+  name: {type:String, unique: true},
   productType: String,
   price: Number,
   inStock: Boolean,
--- app.js
+++ app.js
@@ -2,6 +2,8 @@ let express = require('express');
 let app = express();
 let mongoose = require('mongoose');
 
+let Product = require('./api/models/productModel');
+
 let port = process.env.PORT || 3000;
 
 let db = mongoose.connect('mongodb://localhost:27017/node-api', (err) => {
@@ -13,6 +15,52 @@ let db = mongoose.connect('mongodb://localhost:27017/node-api', (err) => {
 app.get('/', (req, res) => {
 
   res.send('hello');
+});
+
+app.get('/createProducts', (req, res) => {
+
+  let productList = [
+    {
+      'name': 'nike top',
+      'productType': 't-shirt',
+      'price': 10.99,
+      'inStock': true,
+      'dateAdded': Date.now()
+    },
+    {
+      'name': 'hat',
+      'productType': 'big hat',
+      'price': 20.99,
+      'inStock': true,
+      'dateAdded': Date.now()
+    },
+    {
+      'name': 'high tops',
+      'productType': 'shoes',
+      'price': 60.99,
+      'inStock': true,
+      'dateAdded': Date.now()
+    },
+    {
+      'name': 'socks',
+      'productType': 'socks',
+      'price': 1.99,
+      'inStock': false,
+      'dateAdded': Date.now()
+    }
+  ];
+
+  Product.create(productList, (err, products) => {
+    // if(err) res.sendStatus(500);
+  });
+});
+
+app.get('/products', (req, res) => {
+
+  Product.find((err, products) => {
+    if(err) res.sendStatus(500);
+    res.send(products);
+  });
 });
 
 app.listen(port, (err) => {
