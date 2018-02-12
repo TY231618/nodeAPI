ee897640ca76c0ce77d2662cad68f99ca48eb00d
Tony <anthony.c.young18@gmail.com>
2018-02-12 10:58:00 +0000

    basic db connection

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 let express = require('express');
-
 let app = express();
+let mongoose = require('mongoose');
 
 let port = process.env.PORT || 3000;
+
+let db = mongoose.connect('mongodb://localhost:27017/node-api', (err) => {
+  if(err) console.log(err);
+
+  console.log('mongoose connected');
+});
 
 app.get('/', (req, res) => {
 
